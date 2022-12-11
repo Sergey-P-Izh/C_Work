7a1401431c8f4cbf1af4c9974d298a0080ddc9f9
Sergey <u3jiom@bk.ru>
2022-12-11 21:37:14 +0400

    Работа с текстом

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ int elements = int.Parse(Console.ReadLine());
 string[] Array = new string[elements];
 for (int i = 0; i < Array.Length; i++)
 {
-Console.Write("Введите строку №{0}:\r\n ", i + 1);
+Console.Write("do №{0}:\r\n ", i + 1);
 Array[i] = Console.ReadLine();
 }
 Console.WriteLine("Вывод массива:");

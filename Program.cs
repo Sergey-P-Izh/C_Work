399ebd437a2fcd945f68a7f382bdfdab0ce1dfee
Sergey <u3jiom@bk.ru>
2022-12-11 18:46:24 +0400

    Подготовлено решение задачи и сохранено

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите количество строк: ");
+int elements = int.Parse(Console.ReadLine());
+string[] Array = new string[elements];
+for (int i = 0; i < Array.Length; i++)
+{
+Console.Write("Введите строку №{0}:\r\n ", i + 1);
+Array[i] = Console.ReadLine();
+}
+Console.WriteLine("Вывод массива:");
+for (int i = 0; i < Array.Length; i++)
+{
+Console.WriteLine(Array[i]);
+}
+
+var newArray = Array.Where(x => x.Length <= 3).ToArray();
+for (int i = 0; i < newArray.Length; i++)
+{
+Console.WriteLine(newArray[i]);
+}
+
+Console.ReadLine();
